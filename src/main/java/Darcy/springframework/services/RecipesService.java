7e9533527159b7c64a07b475c4932dc34f65defd
darcyxian <darcyxianau@gmail.com>
2020-08-17 14:32:20 +1000

    try to solve ci bug

--- src/main/java/Darcy/springframework/services/RecipesService.java
+++ src/main/java/Darcy/springframework/services/RecipesService.java
@@ -2,7 +2,7 @@ package Darcy.springframework.services;
 
 import Darcy.springframework.domain.Recipe;
 
-import java.util.Set;
+
 
 public interface RecipesService {
     Iterable<Recipe> getRecipe();
