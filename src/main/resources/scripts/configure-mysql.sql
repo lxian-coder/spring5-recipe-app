f5e63a7a99c4028c408042f35fae7bcbd1a29af5
darcyxian <darcyxianau@gmail.com>
2020-09-22 16:59:27 +1000

    added circle

---
+++ src/main/resources/scripts/configure-mysql.sql
@@ -0,0 +1,37 @@
+
+
+
+## Use to run mysql db docker image, optional if you are not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+# create database service accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'darcy';
+CREATE USER 'sfg_prod_user'@'localhost' INDENTIFIED BY 'darcy';
+# It means from any host. when you connect to the Docker container, you are going to be coming from a different IP address.
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'darcy';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'darcy';
+
+
+# Database grants
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
+
